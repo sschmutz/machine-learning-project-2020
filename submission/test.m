97209117567097b63e0ffd13c610447e3da1a909
sschmutz <stefan.schmutz@outlook.com>
2020-11-29 21:16:31 +0100

    working MATLAB code for naive bayes train and test

--- submission/test.m
+++ submission/test.m
@@ -1,1 +1,38 @@
-test_data = readtable("data/test.csv");
+%% reading test features (headline_id, word_id, word_count)
+
+test_features = dlmread('data/test_features.csv', ',', 1, 0);
+
+
+%% reading test lables
+
+test_labels = readtable('data/test_labels.csv');
+
+test_labels_20min = test_labels{:,"source"} == "20min";
+
+indices_20min = find(test_labels_20min == 1);
+indices_nzz = find(test_labels_20min == 0);
+
+
+%% create document-term-matrix
+
+num_test_headlines = 21827;
+num_vocabulary_words = 44108;
+
+test_matrix_sparse = sparse(test_features(:,1), ... 
+                            test_features(:,2), ... 
+                            test_features(:,3), ...
+                            num_test_headlines, num_vocabulary_words);
+                         
+test_matrix = full(test_matrix_sparse);
+
+
+%% calculate predictions
+
+log_20min = test_matrix*(log(class_prob_20min))' + log(prior_prob_20min);
+log_nzz = test_matrix*(log(class_prob_nzz))'+ log(prior_prob_nzz);
+
+test_predictions_20min = log_20min > log_nzz;
+
+%% calculate test accuracy
+
+mean(test_labels_20min == test_predictions_20min)
--- submission/train.m
+++ submission/train.m
@@ -1,44 +1,46 @@
-%% reading train data set (text, source, headline_id)
-train_data = readtable('data/train.csv');
+%% reading train features (headline_id, word_id, word_count)
 
-%% calculate class probabilities
-labels = train_data{:,'source'};
+train_features = dlmread('data/train_features.csv', ',', 1, 0);
 
-n_nzz = sum(labels == "nzz");
-n_20min = sum(labels == "20min");
 
-prior_nzz = n_nzz/(n_nzz+n_20min);
-prior_20min = n_20min/(n_nzz+n_20min);
+%% reading train lables
 
-%% create word vocabulary
-train_data_size = size(train_data);
-train_data_rows = train_data_size(1);
+train_labels = readtable('data/train_labels.csv');
 
-vocabulary = containers.Map('KeyType', 'char', 'ValueType', 'uint32');
+train_labels_20min = train_labels{:,"source"} == "20min";
 
-for row = 1:3 %train_data_rows
-    words = split(train_data{row, 'text'});
-    
-    for word_index = 1:length(words)
-        word = words{word_index};
-        
-        if isKey(vocabulary, word)
-            vocabulary(word) = vocabulary(word)+1;
-        else
-            vocabulary(word) = 1;
-            
-        end
-    end
-end
+indices_20min = find(train_labels_20min == 1);
+indices_nzz = find(train_labels_20min == 0);
 
-word = keys(vocabulary)';
-count = values(vocabulary)';
-vocabulary_table = table(word, count, 'VariableNames', {'word' 'count'});
-vocabulary_table = sortrows(vocabulary_table, 'count', 'descend');
-
-vocabulary_id = (1:size(vocabulary_table))';
-
-vocabulary_table = addvars(vocabulary_table, vocabulary_id);
 
 %% create document-term-matrix
 
+num_train_headlines = 65482;
+num_vocabulary_words = 44108;
+
+train_matrix_sparse = sparse(train_features(:,1), ... 
+                             train_features(:,2), ... 
+                             train_features(:,3), ...
+                             num_train_headlines, num_vocabulary_words);
+                         
+train_matrix = full(train_matrix_sparse);
+
+
+%% calculate prior probabilities
+
+prior_prob_20min = length(indices_20min)/num_train_headlines;
+prior_prob_nzz = 1-prior_prob_20min;
+
+
+%% calculate class conditional probabilities
+
+% get number of words for each headline
+headlines_words = sum(train_matrix, 2);
+
+% sum up words per class
+wordcount_20min = sum(headlines_words(indices_20min));
+wordcount_nzz = sum(headlines_words(indices_nzz));
+
+% calculate class conditional probabilities
+class_prob_20min = (sum(train_matrix(indices_20min, :)) + 1)./(wordcount_20min + num_train_headlines);
+class_prob_nzz = (sum(train_matrix(indices_nzz, :)) + 1)./(wordcount_nzz + num_train_headlines);
