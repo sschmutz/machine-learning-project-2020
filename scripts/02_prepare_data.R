324471f0ce5bfea560c7d62feeadf295c5a49bc3
sschmutz <stefan.schmutz@outlook.com>
2020-11-29 21:16:00 +0100

    rewrite script to get from raw data to data we can use with MATLAB

--- scripts/02_prepare_data.R
+++ scripts/02_prepare_data.R
@@ -8,13 +8,13 @@ library(here)
 
 headlines_20min <-
   read_csv(here("data", "headlines_20min.csv")) %>%
-  select("text") %>%
+  select(text, object) %>%
   mutate(source = "20min") %>%
   unique()
 
 headlines_nzz <-
   read_csv(here("data", "headlines_nzz.csv")) %>%
-  select("text") %>%
+  select(text, object) %>%
   mutate(source = "nzz") %>%
   unique()
 
@@ -28,23 +28,78 @@ headlines <-
   mutate(headline_id = 1:n())
 
 
+# create vocabulary -------------------------------------------------------
+
+# define minimum occurrences required for a word to be included in vocabulary
+min_word_count <- 2
+
+vocabulary <-
+  headlines %>%
+  unnest_tokens(word, text) %>%
+  count(word, sort = TRUE, name = "word_count") %>%
+  rowid_to_column("word_id") %>%
+  filter(word_count >= min_word_count)
+
+vocabulary_stop_words <-
+  vocabulary %>%
+  inner_join(stop_words_german, by = "word") %>%
+  select(-word_count)
+
+
 # split training and test data --------------------------------------------
 
 train_fraction <- 0.75
 
 set.seed(20201122)
+
 train <-
   headlines %>%
-  sample_frac(train_fraction)
+  sample_frac(train_fraction) %>%
+  arrange(headline_id) %>%
+  rowid_to_column("train_id") 
+
+train_labels <-
+  train %>%
+  select(train_id, source, object)
+
+train_features <-
+  vocabulary %>%
+  select(word_id, word) %>%
+  inner_join(unnest_tokens(train, word, text), by = "word") %>%
+  select(train_id, word_id) %>%
+  group_by(train_id, word_id) %>%
+  summarise(word_count = n())
+
 
 test  <- 
   headlines %>%
-  anti_join(train, by = "headline_id")
+  anti_join(train, by = "headline_id") %>%
+  arrange(headline_id) %>%
+  rowid_to_column("test_id") 
+
+test_labels <-
+  test %>%
+  select(test_id, source, object)
+
+test_features <-
+  vocabulary %>%
+  select(word_id, word) %>%
+  inner_join(unnest_tokens(test, word, text), by = "word") %>%
+  select(test_id, word_id) %>%
+  group_by(test_id, word_id) %>%
+  summarise(word_count = n())
 
 
 
 # write datasets ----------------------------------------------------------
 
-write_csv(test, here("submission", "data", "test.csv"))
-write_csv(train, here("submission", "data", "train.csv"))
-write_csv(stop_words_german, here("submission", "data", "stop_words_german.csv"))
+write_csv(vocabulary, here("submission", "data", "vocabulary.csv"))
+write_csv(vocabulary_stop_words, here("submission", "data", "vocabulary_stop_words.csv"))
+
+write_csv(train_labels, here("submission", "data", "train_labels.csv"))
+write_csv(train_features, here("submission", "data", "train_features.csv"))
+
+write_csv(test_labels, here("submission", "data", "test_labels.csv"))
+write_csv(test_features, here("submission", "data", "test_features.csv"))
+
+
