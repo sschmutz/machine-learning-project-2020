0274bb1f55138535e63cf23cc71e78cd4e8d1933
sschmutz <stefan.schmutz@outlook.com>
2020-11-29 16:16:59 +0100

    add comments

--- submission/document_term_matrix.m
+++ submission/document_term_matrix.m
@@ -1,5 +1,5 @@
-function [DTM] = document_term_matrix (dataset, exclude_stop_words)
-% TODO: this function should return a document term matrix of a table
-% if exclude_stop_words is TRUE, those will be removed
+function [DTM, dictionary] = document_term_matrix (dataset, exclude_stop_words)
+% TODO: this function should return a document term matrix and dictionary
+% of a table. If exclude_stop_words is TRUE, those will be removed
 
 end
---
+++ submission/test.m
@@ -0,0 +1,1 @@
+test_data = readtable("data/test.csv");
