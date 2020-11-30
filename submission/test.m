6a98b60b1dd4803cb566ae9b2e19290198cc7602
sschmutz <stefan.schmutz@outlook.com>
2020-11-30 09:34:30 +0100

    rewrite train and test to include specified number of headlines and vocabulary

--- submission/test.m
+++ submission/test.m
@@ -1,22 +1,32 @@
 %% reading test features (headline_id, word_id, word_count)
 
+num_test_headlines = 21827;
+num_vocabulary_words = 44108;
+
 test_features = dlmread('data/test_features.csv', ',', 1, 0);
 
+% select maximum num_test_headlines with a maximum vocabulary size
+% of num_vocabulary_words
+
+selected_headlines = 1:num_test_headlines;
+
+test_indices = ismember(test_features(:,1), selected_headlines) & ...
+                test_features(:,2) <= num_vocabulary_words;
+
+test_features = test_features(test_indices,:);
 
 %% reading test lables
 
 test_labels = readtable('data/test_labels.csv');
 
 test_labels_20min = test_labels{:,"source"} == "20min";
+test_labels_20min = test_labels_20min(1:num_test_headlines);
 
 indices_20min = find(test_labels_20min == 1);
 indices_nzz = find(test_labels_20min == 0);
 
 
 %% create document-term-matrix
-
-num_test_headlines = 21827;
-num_vocabulary_words = 44108;
 
 test_matrix_sparse = sparse(test_features(:,1), ... 
                             test_features(:,2), ... 
--- submission/train.m
+++ submission/train.m
@@ -1,6 +1,19 @@
 %% reading train features (headline_id, word_id, word_count)
 
+num_train_headlines = 65482;
+num_vocabulary_words = 44108;
+
 train_features = dlmread('data/train_features.csv', ',', 1, 0);
+
+% select maximum num_train_headlines with a maximum vocabulary size
+% of num_vocabulary_words
+
+selected_headlines = 1:num_train_headlines;
+
+train_indices = ismember(train_features(:,1), selected_headlines) & ...
+                train_features(:,2) <= num_vocabulary_words;
+
+train_features = train_features(train_indices,:);
 
 
 %% reading train lables
@@ -8,15 +21,13 @@ train_features = dlmread('data/train_features.csv', ',', 1, 0);
 train_labels = readtable('data/train_labels.csv');
 
 train_labels_20min = train_labels{:,"source"} == "20min";
+train_labels_20min = train_labels_20min(1:num_train_headlines);
 
 indices_20min = find(train_labels_20min == 1);
 indices_nzz = find(train_labels_20min == 0);
 
 
 %% create document-term-matrix
-
-num_train_headlines = 65482;
-num_vocabulary_words = 44108;
 
 train_matrix_sparse = sparse(train_features(:,1), ... 
                              train_features(:,2), ... 
@@ -42,5 +53,6 @@ wordcount_20min = sum(headlines_words(indices_20min));
 wordcount_nzz = sum(headlines_words(indices_nzz));
 
 % calculate class conditional probabilities
+% we add 1 to each wordcount to avoid probabilities of 0
 class_prob_20min = (sum(train_matrix(indices_20min, :)) + 1)./(wordcount_20min + num_train_headlines);
 class_prob_nzz = (sum(train_matrix(indices_nzz, :)) + 1)./(wordcount_nzz + num_train_headlines);
